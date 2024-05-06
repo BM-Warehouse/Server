3032fa75dfe976f75f84a00f90dd531397ddb2b3
Febrio Evan Hartanto <febrioevanhartanto@gmail.com>
2024-05-06 20:12:07 +0800

    feat: add basic crud for warehouses

---
+++ src/controllers/warehouse.controller.js
@@ -0,0 +1,59 @@
+const WarehouseService = require('@services/warehouse.service');
+
+class WarehouseController {
+  static async getAllWarehouses(req, res, next) {
+    try {
+      const warehouses = await WarehouseService.getAllWarehouses();
+      res.status(200).json(warehouses);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  static async getWarehouseDetail(req, res, next) {
+    try {
+      const { id } = req.params;
+      const warehouse = await WarehouseService.getWarehouseDetail(id);
+      res.status(200).json(warehouse);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  static async addWarehouse(req, res, next) {
+    try {
+      const { name, address, city } = req.body;
+      const newWarehouse = await WarehouseService.addWarehouse(name, address, city);
+      res
+        .status(201)
+        .json({ message: 'Added a new warehouse successfully!' }, { data: newWarehouse });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  static async editWarehouse(req, res, next) {
+    try {
+      const { id } = req.params;
+      const { name, address, city } = req.body;
+      const updatedWarehouse = await WarehouseService.editWarehouse(id, name, address, city);
+      res
+        .status(200)
+        .json({ message: 'Warehouse Data Updated Successfully!', data: updatedWarehouse });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  static async deleteWarehouse(req, res, next) {
+    try {
+      const { id } = req.params;
+      await WarehouseService.deleteWarehouse(id);
+      res.status(200).json({ message: 'Warehouse Deleted Successfully!' });
+    } catch (e) {
+      next(e);
+    }
+  }
+}
+
+module.exports = WarehouseController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const productRoute = require('@routes/product.route');
 const categoryRoute = require('@routes/category.route');
+const warehouseRoute = require('@routes/warehouse.route');
 
 /**** Koleksi Route disini *******/
 router.use('/api/products', productRoute);
 router.use('/api/categories', categoryRoute);
+router.use('/api/warehouses', warehouseRoute);
 
 module.exports = router;
