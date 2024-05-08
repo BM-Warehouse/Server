f971ad92e637930118452686880cc6b4bd6d8d8a
AlexWiraWilantapoera <alexwira99@gmail.com>
2024-05-08 16:59:16 +0700

    feat: pagination

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -1,10 +1,23 @@
+const { getPaginationStatus } = require('@src/libs/pagination');
 const CategoryService = require('@src/services/category.service');
+
+const DEFAULT_PAGE = 1;
+const DEFAULT_LIMIT = 10;
 
 class CategoryController {
   static async getAllCategories(req, res, next) {
     try {
-      const categories = await CategoryService.getAllCategories();
-      res.status(200).json(categories);
+      let { page, limit } = req.query;
+      if (!page) page = DEFAULT_PAGE;
+      if (!limit) limit = DEFAULT_LIMIT;
+      page = +page;
+      limit = +limit;
+      const categories = await CategoryService.getAllCategories({ page, limit });
+      const totalCount = await CategoryService.getAllCount();
+      const pagination = getPaginationStatus(page, limit, totalCount);
+      res
+        .status(200)
+        .json({ message: 'Get all categories success', categories: categories, pagination });
     } catch (e) {
       next(e);
     }
