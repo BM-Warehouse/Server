f85c4c231341bd2a78f475ed25e1f74fe3e2883f
ricyoamb <rickyyohanesambarita@gmail.com>
2024-05-10 20:43:25 +0700

    feat: error handler in users

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,37 +1,39 @@
 const jwt = require('@libs/jwt');
 const AuthService = require('@services/auth.service');
 
-const authentication = async (req, res, next) => {
-  if (req.headers.authorization) {
-    const accessToken = req.headers.authorization.split(' ')[1];
+class AuthMiddleware {
+  static async authentication(req, res, next) {
+    if (req.headers.authorization) {
+      const accessToken = req.headers.authorization.split(' ')[1];
 
-    try {
-      const decoded = await jwt.verifyToken(accessToken);
-      const result = await AuthService.findUserById(decoded.id);
-      if (!result) {
-        throw new Error('User not found');
+      try {
+        const decoded = await jwt.verifyToken(accessToken);
+        const result = await AuthService.findUserById(decoded.id);
+        if (!result) {
+          throw new Error('User not found');
+        }
+        req.loggedUser = {
+          id: result.id,
+          username: result.username,
+          role: result.role,
+        };
+        next();
+      } catch (error) {
+        next(error);
       }
-      req.loggedUser = {
-        id: result.id,
-        username: result.username,
-        role: result.role,
-      };
-      next();
-    } catch (error) {
-      next(error);
+    } else {
+      // Jika tidak ada token diberikan, kirim tanggapan JSON 'Unauthorized'
+      res.status(401).json({ error: 'Unauthorized' });
+      return;
     }
-  } else {
-    // Jika tidak ada token diberikan, kirim tanggapan JSON 'Unauthorized'
-    res.status(401).json({ error: 'Unauthorized' });
-    return;
   }
-};
 
-const authorization = async (req, res, next) => {
-  if (req.loggedUser.role === 'admin') next();
-  else {
-    throw new Error('Unauthorized');
+  static async authorization(req, res, next) {
+    if (req.loggedUser.role === 'admin') next();
+    else {
+      throw new Error('Unauthorized');
+    }
   }
-};
+}
 
-module.exports = { authentication, authorization };
+module.exports = AuthMiddleware;
